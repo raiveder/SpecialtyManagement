3fcca3a31d2f8b3f0304f08cb49804903b8b6c3b
Дмитрий Сергеев <kolovrat-86@yandex.ru>
2023-05-25 18:44:29 +0300

    Юнит-тесты

---
+++ SpecialtyManagementTest/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("SpecialtyManagementTest")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("SpecialtyManagementTest")]
+[assembly: AssemblyCopyright("Copyright ©  2023")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("9033f341-93e5-4b0a-ad4d-6752c62db056")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ SpecialtyManagementTest/UnitTest.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using SpecialtyManagement;
+using System;
+
+namespace SpecialtyManagementTest
+{
+    [TestClass]
+    public class UnitTest
+    {
+        [TestMethod]
+        public void GetStudentsFromFile_EqualResultZeroWithFileNotFound()
+        {
+            Assert.AreEqual(0, Students.GetStudentsFromFile("testFile.test").Count);
+        }
+
+        [TestMethod]
+        public void GetYearAndSemester_TrueResultForCurrentSemester()
+        {
+            bool result = false;
+            Arrears.GetYearAndSemester(out int year, out int semester, true);
+            if (year == DateTime.Now.Year - 1 && semester == 2)
+            {
+                result = true;
+            }
+
+            Assert.IsTrue(result);
+        }
+
+        [TestMethod]
+        public void GetYearAndSemester_TrueResultForLastSemester()
+        {
+            bool result = false;
+            Arrears.GetYearAndSemester(out int year, out int semester, false);
+            if (year == DateTime.Now.Year - 1 && semester == 1)
+            {
+                result = true;
+            }
+
+            Assert.IsTrue(result);
+        }
+    }
+}
